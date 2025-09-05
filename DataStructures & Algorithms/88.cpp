899f2982a44cef9225f68beb8ba07872fd876db8
Akshay Lende <akshaylende48@gmail.com>
2025-09-05 22:20:55 +0530

    88 - Two pointers Approach

--- DataStructures & Algorithms/88.cpp
+++ DataStructures & Algorithms/88.cpp
@@ -13,8 +13,11 @@ Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 # Query/ Code - */
-
 // Write your code below this: - 
+# Time Complexity - O(N log(m+n))
+
+# Space Complexity - O(m+n)
+
 #include<bits/stdio.h>
 using namespace std; 
 
@@ -51,7 +54,7 @@ void Show(vector<int>& nums){
 }
 
 int main(){
-    vector<int> nums1([1, 2, 3, 0, 0, 0]), nums2([4, 5, 6]);
+    vector<int> nums1([1, 2, 3, 0, 0, 0]), nums2([4, 5, 6];
     // Edit your main code here 
 
     mySort(nums1, nums2);
@@ -59,3 +62,41 @@ int main(){
     Show(nums1);
     return 0;
 }
+
+
+###### Approach - 2 ########
+/*
+optimized Approach - 
+step 1 - using two pointers approach and declaring a variable to k to reference a position in the nums1[] array 
+step 2 - placing i on m-1 and j on n-1 and k on the end of the array and starts traversing from the end to start. 
+Step 3 - Comparing i && j indexed variable at every step and deciding the values for index k till the start of the array. 
+
+TC - O(m+n)
+SC - O(1)
+
+*/
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int k=m+n-1, i = m-1, j=n-1;
+        //
+        while((i >= 0 && j>=0)){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                nums1[i]=0;
+                i--;
+            }
+            else{ 
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0){
+            nums1[k--] = nums2[j--];
+        }
+    }
+};

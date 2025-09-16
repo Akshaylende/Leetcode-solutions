d0be1ffba65d1ae10370f92c1caa4f4a61868807
Akshay Lende <akshaylende48@gmail.com>
2025-09-16 22:09:23 +0530

    73 - Optimized Approach

--- DataStructures & Algorithms/73.cpp
+++ DataStructures & Algorithms/73.cpp
@@ -39,3 +39,52 @@ public:
         }
     }
 };
+
+
+
+#optimized O(1) Approach 
+TC - O(m*n)
+SC - O(1)
+// marking the first row and column to be zero and then replicating it 
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int first_cell = 1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    if(i == 0){
+                        first_cell = 0;
+                    }
+                    else{
+                        matrix[i][0] = 0; // row
+                        matrix[0][j] = 0; // col 
+                    }
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+                if(matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0){
+            for(int i=0; i<m;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(first_cell == 0){
+            for(int j = 0; j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        
+    }
+};

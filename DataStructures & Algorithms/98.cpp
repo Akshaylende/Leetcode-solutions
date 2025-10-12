c64e7dcb276df9a276ea284835eb39b13aa2a9da
Akshay Lende <akshaylende48@gmail.com>
2025-10-12 09:41:12 +0530

    98 - Validate Binary Search Tree

---
+++ DataStructures & Algorithms/98.cpp
@@ -0,0 +1,82 @@
+/*
+## 98. Validate Binary Search Tree ##
+
+
+# Problem Link - 
+https://leetcode.com/problems/validate-binary-search-tree/?envType=study-plan-v2&envId=top-interview-150
+
+# Problem Statement -
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys strictly less than the node's key.
+The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+# Query/ Code - 
+*/
+// TC - O(N)
+// SC - O(N)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> traversal;
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        traversal.push_back(root->val);
+        inorder(root->right);
+    }
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        int i = 0 , j = i+1;
+        while(j < traversal.size()){
+            if(traversal[i] >= traversal[j]) return false;
+            i++, j++;
+        } 
+        return true;
+    }
+};
+
+
+
+
+// optimized approach 
+
+// shrinking boundaries and expanding min range
+//TC - O(N)
+// SC - O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* root, long long mina, long long maxa){
+        if(!root) return true;
+        if(root->val <= mina || root->val >= maxa) return false;
+        return check(root->left, mina, root->val) && check(root->right, root-> val, maxa);
+    }
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};

9c86dff17de1bed80443f8b07a83081472703095
Akshay Lende <akshaylende48@gmail.com>
2025-10-18 01:02:18 +0530

    74 - Search in a 2D matrix

---
+++ DataStructures & Algorithms/74.cpp
@@ -0,0 +1,45 @@
+/*
+## 74. Search a 2D Matrix ##
+
+
+# Problem Link - 
+https://leetcode.com/problems/search-a-2d-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+
+# Problem Statement -
+You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+
+# Query/ Code - 
+*/
+// TC - O(N logn)
+// SC - O(1)
+
+class Solution {
+public:
+    bool binarySearch(vector<int>& arr, int low, int high, int target){
+        //cout<< low << " "<< high<< endl;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(arr[mid] == target) return true;
+            if(arr[mid]>target) high = mid-1;
+            else low = mid+1;
+        }
+        return false; 
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0; i<m; i++){
+            if(matrix[i][n-1]>= target){
+                return binarySearch(matrix[i], 0, n-1, target);               
+            }
+        }
+        return false;
+    }
+};
+

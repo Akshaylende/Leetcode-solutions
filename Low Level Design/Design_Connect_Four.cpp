a2f496f0f864703ef663837db22a626ceb55e23a
Akshay Lende <akshaylende48@gmail.com>
2026-02-05 17:58:44 +0530

    Design Connect Four

--- Low Level Design/Design_Connect_Four.cpp
+++ Low Level Design/Design_Connect_Four.cpp
@@ -1,12 +1,12 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Definition of class player
+
+// Defining player
 class Player{
     string name;
     int piece;
-
-    public:    
+    public:
         Player(string name, int color){
             this->name = name;
             this->piece = color;
@@ -18,180 +18,181 @@ class Player{
 
         int getPiece(){
             return this->piece;
-        } 
+        }
 };
 
 
-// Definition of class Board
+
+// Defining Board
 class Board{
+    vector<vector<int>> Grid;
     int rows, cols, connectN;
-    vector<vector<int>> grid;
-
     public:
-        Board(int rows, int cols, int connectN){
-            this->rows = rows;
-            this->cols = cols;
-            this->connectN = connectN;
-            this->grid.resize(rows, vector<int> (cols));
-            FillBoard(grid, 0);
+        Board(int row, int col, int connect){
+            this->rows = row;
+            this->cols = col;
+            this->connectN = connect;
+            Grid.resize(rows, vector<int>(cols));
+            fillBoard( 0);
         }
 
-        void FillBoard(vector<vector<int>> &grid, int piece){
-            for(int i =0; i< rows; i++){
-                for(int j = 0; j < cols; j++){
-                    grid[i][j] = piece;
-                }
-            }            
-        }
-
-        vector<vector<int>> getBoard(){
-            return this->grid;
-        }
-
-        int getColumns(){
-            return this->cols;
-        }
-
-        int placepiece(int col, int piece){
-            if(col < 0 && col >= this->cols)
-                return -1;
-            else if(piece == 0)
-                return -1;
-            for(int i = rows-1; i >= 0; i--){
-                if(grid[i][col] == 0){
-                    grid[i][col] = piece;
-                    return i;
-                }
-            } 
-            return -1;
-        }
-
-        bool checkWinner(int row, int col, int piece){
-            int count = 0;
-            // check horizontally
-            for(int j = 0; j< cols; j++){
-                if(grid[row][j] == piece)
-                    count+=1;
-                else
-                    count = 0;
-                if(count == connectN)
-                    return true;
-            }   
-
-            // check vertically
-            count = 0;
-            for(int i=0; i< rows; i++){
-                if(grid[i][col] == piece)
-                    count+=1;
-                else
-                    count = 0;
-                
-                if(count == connectN)
-                    return true;
-            } 
-
-            // check diagonal 
-            count = 0;
-            for(int i = 0; i< rows; i++){
-                int j = row + col - i;
-                if(j>=0 && j < cols && grid[i][j] == piece)
-                    count+=1;
-                else
-                    count = 0;
-                if(count == connectN)
-                    return true; 
-            }
-
-            // anti diagonal 
-            count = 0;
-            for(int i = 0; i< rows; i++){
-                int j = col - row + i;
-                if(j>=0 && j < cols && grid[i][j] == piece)
-                    count+=1;
-                else
-                    count = 0;
-                if(count == connectN)
-                    return true; 
-            }
-
-
-            return false;
-        }        
-
-};
-
-class Game{
-    vector<vector<int>> grid;
-    int connectN, targetS;
-    vector<Player* > players;
-    unordered_map<string , int> scores;
-    
-    public:
-        Game(Board b1, int  connectN, int target){
-            this->grid = b1;
-            this->connectN = connectN;
-            this->targetS = target;
-
-            this->players.push_back(new Player('Player 1', 1));
-            this->players.push_back(new Player('Player 2', 2));
-            
-            for(auto x : players){
-                scores[x->getName()] = 0;
-            }
+        void fillBoard(int val){
+            for(int i=0; i<rows; i++)
+                for(int j=0; j<cols; j++)
+                    Grid[i][j] = val;
         }
 
         void printBoard(){
-            cout<<"Board" <<endl;
-            for(int i = 0; i< grid.size(); i++){
-                for(int j = 0; j< grid[0].size(); j++)
-                    if(grid[i][j] == 1)
-                        cout<< "Y ";
-                    else if(grid[i][j] == 2)
-                        cout<< "R ";
-                    else 
-                        cout<<"0 ";
-                cout<< endl;
+            for(int i =0; i< rows; i++){
+                for(int j =0; j< cols; j++){
+                    if(Grid[i][j] == 1){
+                        cout<<"X ";
+                    } 
+                    else if(Grid[i][j] == 2){
+                        cout<<"Y ";
+                    }
+                    else cout<<"O "; 
+                }
+                cout<<endl;
             }
         }
 
-        pair<int, int> playMove(Player player){
-            printBoard();
-            cout<< player.getName()<< "'s turn"<<endl;
-            cin >> movecol;
-            moverow =  grid.placepiece(movecol, player.getPiece());
-            return {moverow, movecol};
-        }
 
-        Player playRound(){
-            while(true){
-                for(auto player in players){
-                    auto [row, col] = playMove(player);
-                    pieceColor = player.getPiece();
-                    if(grid->checkWinner(row, col, pieceColor)){
-                        scores[player.getName()] +=1;
-                        return player;
-                    }
+        int placePiece(int col, int piece){
+            if(col<0 || col >= cols) return -1;
+            for(int i = rows-1; i>=0; i--){
+                if(Grid[i][col] == 0){
+                    Grid[i][col] = piece;
+                    return i;
                 }
             }
-            return NULL;
+            return -1;
         }
-        
-        void play(){
-            int maxScore = 0;
-            Player winner = NULL;
-            while(maxScore < targetS){
-                winner = playRound();
-                if(winner)
-                    cout<< winner.getName()<<" won this round"<<endl;
-                maxscore = max(maxscore, scores[winner.getName()]);
-                grid.FillBoard(grid, 0); 
+
+        bool checkWin(int row, int col, int piece){
+            int count = 0;
+            // horizontal
+            for(int i=0; i<cols; i++){
+                if(Grid[row][i] == piece) count+=1;
+                else count = 0;
+                if(count == connectN) return true;
             }
-            cout<< winner.getName()<< " won the game"<<endl;
+
+            // vertical
+            count = 0;
+            for(int i=0; i<rows; i++){
+                if(Grid[i][col] == piece) count+=1;
+                else  count = 0;
+                if(count == connectN) return true;
+            }
+
+            // diagonal
+            count = 0;
+            for(int i=0; i<rows; i++){
+                int j = row + col - i;
+                if(j>=0 && j< cols && Grid[i][j] == piece) count+=1;
+                else count = 0;
+                if(count == connectN) return true;
+            }
+
+            // Anti-diagonal
+            count = 0;
+            for(int i=0; i<rows; i++){
+                int j = col - row + i;
+                if(j>=0 && j< cols && Grid[i][j] == piece) count+=1;
+                else count = 0;
+                if(count == connectN) return true;
+            }
+
+            return false;
         }
 };
 
+class Game{
+    int targetScore, connectN;
+    Board *b;
+    vector<Player *> players;
+    unordered_map<string, int> scores;
+    public:
+        Game(Board *b1, int targetS, int connect){
+            this->targetScore = targetS;
+            this->connectN = connect;
+            this->b = b1;
+            // Instantiating players
+            players.push_back(new Player("player 1", 1));
+            players.push_back(new Player("player 2", 2));
+
+            // setting scores
+            scores[players[0]->getName()] = 0;
+            scores[players[1]->getName()] = 0;
+        }
+
+    
+        bool playChance(Player *player){
+            b->printBoard();
+            int col;
+            cout<<"Enter a valid column move(zero Indexed): ";
+            cin>>col;
+            int row = b->placePiece(col, player->getPiece());
+            if(row != -1){
+                if(b->checkWin(row, col, player->getPiece())){
+                    scores[player->getName()] +=1;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        string playRound(){
+            while(true){
+                for(auto player: players){
+                    if(playChance(player))
+                        return player->getName();
+                }
+            }
+            return "";
+        }
+
+        void play(){
+            int round = 0;
+            int maxScore = 0;
+            string winner = "";
+            while(maxScore < targetScore){
+                round += 1;
+                winner = playRound();
+                if(scores.find(winner)!= scores.end()){
+                    scores[winner]+=1;
+                    maxScore = max(maxScore, scores[winner]);
+                    cout<< winner << " won the "<< round << " round"<<endl;
+                }
+                b->fillBoard(0);
+            }
+            cout<< winner << " is the winner of the game."<<endl;
+        }
+
+};
 
 int main(){
+    cout<<"In main"<<endl; 
+    int r, c, cn, ts;
+    cout<< "Enter the rows, cols and connect n count : ";
+    cin>> r>> c>> cn;
+    Board *b1 = new Board(r, c, cn);
+    cout<< endl;
+
+    cout<<"Enter the target score to declare someone as win";
+    cin>> ts; 
+    cout<< endl;
+
+    Game *g1  = new Game(b1, ts, cn);
+    g1->play();
 
     return 0;
 }
+
+
+
+
+
+
+

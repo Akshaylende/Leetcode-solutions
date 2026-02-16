0cf3186f7d97edd3ab4a8bb65c10fc1ff0d521ea
Akshay Lende <akshaylende48@gmail.com>
2026-02-16 17:34:41 +0530

    Movie Recommendation Engine

--- Low Level Design/Movie_Recommendation_Engine.cpp
+++ Low Level Design/Movie_Recommendation_Engine.cpp
@@ -1,179 +1,143 @@
-# include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-
-// Defination of User class
+// Defining User class
 class User{
-  int uid;
   string name;
-  unordered_map<string, int> ratings;
   public:
-    User(int id, string name){
-      this -> uid = id;
-      this -> name = name;
+  unordered_map<string, int> reviews;
+    User(string uname){
+      this->name = uname;
     }
     
-    // Getter for name  
     string getName(){
       return this->name;
     }
     
-    int getRatingsCount(){
-      int count = 0;
-      for(auto [x, y] : ratings){
-        if(y > 0) count++; 
-      }
-      return count;
-    }
-  
-    // ratings handling
-    bool addRatings(string title, int rate){
-      if(ratings.find(title) != ratings.end()
-        || ratings[title] != 0){
-          return false;
-      }
-      ratings[title] = rate;
-      return true;
+    // Function to add user ratings for movies
+    void addReview(string title, int rating){
+      reviews[title] = rating;
     }
     
-    // checking for watched movies
+    // checks for watched movies
     bool hasWatch(string title){
-      
-      if(ratings.find(title) != ratings.end())
-        return true;
-        
-      return false;
+      if(reviews.find(title) == reviews.end())
+        return false;
+      return true;
     }
 };
 
-
-
-
-// Defination for Movie class
+// Defining Movie class
 class Movie{
-  int id;
   string title;
-  unordered_map<int, unordered_set<User *>> ratings_group;
   public:
-    Movie(int id, string name){
-      this->id = id;
+    Movie(string name){
       this->title = name;
     }
     
-    // Getter for movie title
-    string getTitle(){
+    string getMovie(){
       return this->title;
     }
-    
-    // Adding user to Ratings Group
-    void addUserToGroup(User *user, int rate){
-      ratings_group[rate].insert(user);
-    }
 };
 
 
 
-// Defination & Implementation of Recommendation Engine
-class RecommendationEngine{
-    unordered_map<string, pair<int, int>> reviews;
+// Defination for MovieRatings class
+class MovieRatings{
+  unordered_map<string, int> movie_ratings;
   public:
-    RecommendationEngine(){
+    MovieRatings(){}
+    
+    // Adding Ratings in the movie reviews  
+    void addRating(string title, int rating){
+      int avg_rating = (rating + movie_ratings[title])/2;
+      movie_ratings[title] = avg_rating;
+    }
+    
+    // printing movie ratings
+    void printMovieRatings(){
+      for(auto [x, y] : movie_ratings)
+        cout<< x << " "<< y <<endl;
+    }
+    
+    
+    // Return Top Three Movies 
+    vector<string> topThreeMovies(User *user){
+      vector<string>top_three_movies;
+      priority_queue<pair<int, string>>pq;
       
-    } 
-    
-    // Adding reviews
-    void addReviews(string title, int rate){
-      int ratingsCount = 0;
-      int ratings = 0;
-        if(reviews.find(title) != reviews.end()){
-            ratings = reviews[title].first;
-            ratingsCount  = reviews[title].second; 
-        }
-      ratingsCount++;
-      ratings += rate;
-      reviews[title] = {ratings, ratingsCount};
-    }
-    
-    
-    // Top Five Movies
-    vector<string> topFiveMovies(){
-      priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>> > pq;
-      for(auto [x, y] : reviews){
-        pq.push({(y.first/y.second), x});
-        if(pq.size() > 5) pq.pop();
+      
+      for(auto [x, y]: movie_ratings){
+        if(!user->hasWatch(x))
+          pq.push({y, x});
       }
-      vector<string> movies;
-      while(!pq.empty()){
-        movies.push_back(pq.top().second);
+      
+      while(!pq.empty() && top_three_movies.size() < 3){
+        auto [rating, movie] = pq.top();
         pq.pop();
+        top_three_movies.push_back(movie);
       }
-      reverse(movies.begin(), movies.end());
-      return movies;
+      
+      return top_three_movies;
     }
-    
-    
-    // Recommend to new User
-    vector<string> Recommend(User *user){
-        if(user->getRatingsCount() == 0)
-          return topFiveMovies();
-        unordered_set<string> movies;
-        
-    }
-    
 };
 
 
-// Main method
-int main(){
-  cout<<"In Main"<<endl;
-  
-  RecommendationEngine RE;
-  
-  Movie *m1 = new Movie(1, "Avengers");
-  Movie *m2 = new Movie(2, "End Game");
-  Movie *m3 = new Movie(3, "Infinity War");
-  Movie *m4 = new Movie(4, "Captain America");
-  Movie *m5 = new Movie(5, "Marvels");
-  Movie *m6 = new Movie(6, "SpiderMan");
-  
-  User *u1 = new User(1, "Akshay");
-  User *u2 = new User(2, "Piyush");
-  User *u3 = new User(3, "Ayush");
-  User *u4 = new User(4, "Aman");
-  User *u5 = new User(5, "Nikki");
-  
-  u1->addRatings("Captain America", 4);
-  u1->addRatings("Marvels" , 4);
-  u1->addRatings("Infinity War", 5);
-  RE.addReviews("Captain America", 4);
-  RE.addReviews("Marvels", 4);
-  RE.addReviews("Infinity War", 5);
-  
-  
-  u2->addRatings("Marvels", 4);
-  u2->addRatings("SpiderMan", 4);
-  RE.addReviews("SpiderMan", 4);
-  RE.addReviews("Marvels", 4);
-  
-  u3->addRatings("Infinity War", 5);
-  u3->addRatings("End Game", 5);
-  RE.addReviews("Infinity War", 5);
-  RE.addReviews("End Game", 5);
-  
-  u4->addRatings("SpiderMan", 4);
-  u4->addRatings("Captain America", 4);
-  RE.addReviews("Captain America", 4);
-  RE.addReviews("SpiderMan", 4);
 
-  
-  vector<string> movies = RE.Recommend(u5);
-  
-  for(auto x: movies){
-    cout<< x << endl;
-  }
-  if(movies.size() == 0){
-    cout<<"Nothing to Recommend"<<endl;
+// Defination of RecomendationEngine
+class RecomendationEngine{
+  MovieRatings *mr;
+  public:
+  RecomendationEngine(){
+    this->mr = new MovieRatings();
   }
   
+  void addMovieRating(string title, int rate){
+    mr->addRating(title, rate);  
+  }
+  
+  void printRecommendations(vector<string>& recomendations){
+    cout<< "Top three Recomendations are ";
+    
+    for(auto x: recomendations)
+      cout<< x << "  ";
+    
+    cout<<endl;
+  }
+  
+  void recomendMovies(User *user){
+      vector<string> list = mr->topThreeMovies(user);
+      printRecommendations(list);
+  }
+};
+
+// Main Method
+int main(){
+  cout<< "Welcome to Recommendation Engine!" << endl;
+  RecomendationEngine *RE = new RecomendationEngine();
+  
+  User *u1 = new User("Akshay");
+  User *u2 = new User("Piyush");
+  User *u3 = new User("Akash");
+  
+  Movie *m1 = new Movie("Avengers");
+  Movie *m2 = new Movie("Spiderman");
+  Movie *m3 = new Movie("Antman");
+  Movie *m4 = new Movie("Ironman");
+  Movie *m5 = new Movie("Marvels");
+  
+  u1->addReview("Antman", 4.5);
+  u3->addReview("Antman", 4);
+  RE->addMovieRating("Antman", 4.5);
+  RE->addMovieRating("Antman", 4);
+  
+  u1->addReview("Ironman", 4);
+  RE->addMovieRating("Ironman", 4);
+  
+  u3->addReview("Spiderman", 4.5);
+  RE->addMovieRating("Spiderman", 4.5);
+  
+  
+  RE->recomendMovies(u1);
   return 0;
 }
